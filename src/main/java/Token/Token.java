00d2b526689bfe8ad982a76e0d87a19797747c6f
Eric Foster <109866223+eric-foster@users.noreply.github.com>
2024-04-14 00:27:31 -0400

    Delete src/main/java/Token directory

--- src/main/java/Token/Token.java
+++
@@ -1,9 +0,0 @@
-package Token;
-
-public class Token {
-    final private String TOKEN = "MTIyMzExMTE5OTcwOTcyODgzOA.GlawSr.SGbcgnzMKfeZ_fW2mPh9rv8LfZ3-OgGlC9H9kg";
-
-    public String getTOKEN() {
-        return TOKEN;
-    }
-}
