a6e015643296d76b0fab4ddc39502c4be9fc7614
Eric Foster <etfoster.1001@yahoo.com>
2024-05-02 21:45:38 -0400

    Updated Welcome Message with new message material

--- src/main/java/WelcomeSystem/WelcomeEmbed.java
+++ src/main/java/WelcomeSystem/WelcomeEmbed.java
@@ -21,9 +21,9 @@ public class WelcomeEmbed {
 
         embedBuilder.setTitle("**Welcome to " + event.getGuild().getName() + "**");
         embedBuilder.setColor(new Color(0x1FD4B0));
-        embedBuilder.setDescription("A Fivem Server, here to provide you with an exceptional experience in roleplay.");
+        embedBuilder.setDescription("A QBCore FiveM Server based in Maricopa and Yavapai County! We are excited for you to begin your journey!");
         embedBuilder.addField("__**Server Info**__", "Here are some notable things about our server:\n- Custom Vehicles\n- Custom Map\n- Custom Scripts\n- Active Admin and Development Team\n- And Much More Fun To Be Had!!!", false);
-        embedBuilder.addField("__**Notable Channels**__", "Make sure to visit the following channels:\n \n" + event.getGuild().getGuildChannelById(1221826998499016715L).getAsMention() + " - For a detailed view of our server rules in conjunction with Discord ToS and Policies.\n \n" + event.getGuild().getGuildChannelById(1221831760040558753L).getAsMention() + " - For general and off-topic chatting.\n \n" + event.getGuild().getGuildChannelById(1224351115966156951L).getAsMention() + " - For any assistance you may need.", false);
+        embedBuilder.addField("__**Notable Channels**__", "Make sure to visit the following channels:\n \n" + event.getGuild().getGuildChannelById(1221826998499016715L).getAsMention() + " - For a detailed view of our server rules in conjunction with Discord ToS and Policies.\n \n" + event.getGuild().getGuildChannelById(1226199865319751731L).getAsMention() + " - For info on CAD linking and setup.\n \n" + event.getGuild().getGuildChannelById(1221831760040558753L).getAsMention() + " - For general and off-topic chatting.\n \n" + event.getGuild().getGuildChannelById(1226895428688281670L).getAsMention() + " - For in-game photos you would like to showcase.\n \n" + event.getGuild().getGuildChannelById(1224351115966156951L).getAsMention() + " - For any assistance you may need.", false);
         embedBuilder.addField("__**Server Invite Link**__", "In case something happens, or you want ot invite some friends to join you, here is a link to our server: https://discord.gg/G4mN4r5bFM", false);
         embedBuilder.setFooter(bot.getUser().getName(), bot.getAvatarUrl());
 
